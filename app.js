b5ab31059abf04a34ca86e1d6e9399cee935f57b
Jayoooppp <patel.js@somaiya.edu>
2022-07-16 15:22:50 +0530

    Adding the remaining files

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
+// jshint esversion:6
 const express = require("express");
 const parser = require("body-parser");
+const date = require(__dirname + "/date.js");
+console.log(date);
 
 const app = express();
 app.set('view engine' , 'ejs');
 
+
 app.use(parser.urlencoded({extended: true}))
 
-var today = new Date();
-var day;
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+app.use(express.static("public"))
 
+
+const work_items = ["Excel" , "Meeting"];
+
+
+const list_items = ["Buy Food" , "Cook Food" , "Eat Food"];
 
 app.get("/" , function (req , res) { 
-    day = days[today.getDay()]
-    res.render("list" , {whichday:day});
+    
+    res.render("list" , {title:date.getDay() , newitems:list_items} );
+ })
+
+
+
+app.post("/" , function (req , res) { 
+    if(req.body.button === "Work")
+    {
+        work_items.push(req.body.item);
+        res.redirect("/work");
+    }else{
+        list_items.push(req.body.item);
+        res.redirect("/")
+    }
+
+ })
+
+app.get("/work" , function (req , res) { 
+    res.render("list" , {title: "Work" , newitems:work_items});
+ })
+
+app.post("/work" , function (req , res) { 
+    work_items.push(req.body.item);
+    res.redirect("/work")
+ })
+
+app.get("/about" , function (req  , res) { 
+    res.render("about")
  })
 
 
